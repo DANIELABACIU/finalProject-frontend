8ed34b9afc610801d0966742fd09350f65e5fbb7
DANIELABACIU <dyana_dya_89@yahoo.com>
2024-01-09 18:29:32 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "Car Shop",
+  appOwner: "BACIU DIANA",
+  appLogo: "https://www.creativefabrica.com/wp-content/uploads/2019/02/Online-shop-shopping-shop-logo-by-DEEMKA-STUDIO-3.jpg",
+  itemCategories: ['SMALL', 'MEDIUM', 'LARGE']
+};
